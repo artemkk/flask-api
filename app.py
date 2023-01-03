493cf7c29895059b4ecf00572c2330881f97d4b9
Artem Kuryachy <akuryachy@gmail.com>
2023-01-03 16:38:30 -0700

    Connection to MySQL db established

--- app.py
+++ app.py
@@ -5,57 +5,26 @@ import pymysql
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-compression = [
-    {'id': 0,
-     'cement': '540',
-     'blast_furnace_slag': '0',
-     'fly_ash': '0',
-     'water': '162',
-     'superplasticizer': '0',
-     'coarse_aggregate': '1040',
-     'fine_aggregate': '676',
-     'age': '28',
-     'strength': '79.99'},
-    {'id': 1,
-     'cement': '540',
-     'blast_furnace_slag': '0',
-     'fly_ash': '0',
-     'water': '162',
-     'superplasticizer': '2.5',
-     'coarse_aggregate': '1055',
-     'fine_aggregate': '676',
-     'age': '28',
-     'strength': '61.89'},
-    {'id': 2,
-     'cement': '332.5',
-     'blast_furnace_slag': '142.5',
-     'fly_ash': '0',
-     'water': '228',
-     'superplasticizer': '2.5',
-     'coarse_aggregate': '932',
-     'fine_aggregate': '594',
-     'age': '28',
-     'strength': '40.27'}
-]
+
+def dict_factory(cursor, row):
+    d = {}
+
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 
 @app.route('/flask-api/v1/resources/compression/all', methods=['GET'])
 def api_all():
-    return jsonify(compression)
+    
+    conn = pymysql.connect(host="artemkk.mysql.database.azure.com", port=3306, database="concrete", user="azureuser", passwd="Ensiferum1994!", ssl={"fake_flag_to_enable_tls":True})
+    conn.row_factory = dict_factory
 
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM compressive_strength;')
 
-@app.route('/flask-api/v1/resources/compression', methods=['GET'])
-def api_id():
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
+    all_cretes = cur.fetchall()
 
-    results = []
-
-    for elem in compression:
-        if elem['id'] == id:
-            results.append(elem)
-
-    return jsonify(results)
+    return jsonify(all_cretes)
 
 app.run()
