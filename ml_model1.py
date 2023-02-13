c444da16e02fb4b7aa31582dacf17765e194e072
Artem Kuryachy <akuryachy@gmail.com>
2023-02-12 17:50:38 -0700

    Ml_model1 used as Yeh's approx

--- ml_model1.py
+++ ml_model1.py
@@ -3,14 +3,15 @@ from keras.models import Sequential
 from keras.layers import Dense
 import matplotlib.pyplot as plt
 import numpy
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score,mean_squared_error
 from matplotlib import pyplot
 import tensorflow as tf
 from keras.utils import plot_model
+from keras.callbacks import EarlyStopping
 
 # load the training and validation datasets
-TrainingSet = numpy.genfromtxt("./raw-data/model-data-sets/concrete_training_data.csv", delimiter=",", skip_header=True)
-ValidationSet = numpy.genfromtxt("./raw-data/model-data-sets/concrete_validation_data.csv", delimiter=",", skip_header=True)
+TrainingSet = numpy.genfromtxt("./raw-data/model-data-sets/concrete_training_data75.csv", delimiter=",", skip_header=True)
+ValidationSet = numpy.genfromtxt("./raw-data/model-data-sets/concrete_validation_data25.csv", delimiter=",", skip_header=True)
 
 # split into input (X) and output (y) variables
 X1 = TrainingSet[:,0:8]
@@ -21,9 +22,8 @@ Y2 = ValidationSet[:,8]
 
 # define the keras model
 model = Sequential()
-model.add(Dense(12, input_shape=(8,), activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='relu'))
+model.add(Dense(8, input_shape=(8,), activation='relu'))
+model.add(Dense(1, activation='linear'))
 
 # compile the keras model
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(learning_rate=1e-6))
@@ -31,8 +31,11 @@ model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(lear
 # Show model architecture
 plot_model(model, to_file='model1_plot.png', show_shapes=True, show_layer_names=True)
 
+# Patient early stopping
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=130)
+
 # fit the keras model on the dataset
-history = model.fit(X1, Y1, validation_data=(X2, Y2), epochs=250000, batch_size=100)
+history = model.fit(X1, Y1, validation_data=(X2, Y2), epochs=10000000, batch_size=125, verbose=2, callbacks=[es])
 
 # Calculate predictions
 PredTestSet = model.predict(X1)
@@ -72,3 +75,6 @@ print("Training Set Model 1 R-Square=", TestR2Value)
 # Compute R-Square value for validation set
 ValR2Value = r2_score(Y2, ValResults)
 print("Validation Set Model 1 R-Square=", ValR2Value)
+
+ValMSEValue = mean_squared_error(Y2, ValResults)
+print("Validation Set Model 1 MSE=", ValMSEValue)
